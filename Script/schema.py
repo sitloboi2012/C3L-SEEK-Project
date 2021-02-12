e6642940581771c6c02e8e6ee3b2f8865f0d109c
Anh Huy Vo <huyvo6812@gmail.com>
2021-02-12 03:38:44 +0000

    Initial Update

---
+++ Script/schema.py
@@ -0,0 +1,42 @@
+import logging
+import json
+import os
+import gzip
+
+from typing import Dict, Text, Any, Generator
+from io import BytesIO
+from itertools import chain, islice
+from jobPosting_Transformer import AUSJobPosting
+
+
+JobPostingType = Dict[Text, Any]
+JobPostingGeneratorType = Generator[JobPostingType, None, None]
+MetadataType = Dict[Text, Dict[Text,Any]]
+
+class JobPostingCollect(object):
+
+    def __init__(self, num_records: int= 100, filePath: str="/content/data.ldjson"):
+        self.filePath = filePath
+        if num_records > 200:
+            logging.warning("'Cannot provide %s records as a maximum of 200 are available', num_records")
+            num_records = 200
+        #file_name = "/content/data.ldjson"
+        f = open(self.filePath)
+        self.lines = f.read().split("\n")
+        self.transformer = AUSJobPosting(partner_id="VA")
+    
+    def __iter__(self) -> JobPostingGeneratorType:
+        for line in self.lines:
+            if line:
+                yield self.transformer._transform(json.loads(line))
+    
+    @property
+    def metadata(self) -> MetadataType:
+        return {'job postings': {
+            'downloaded_from': 'http://opendata.cs.vt.edu/dataset/openjobs-jobpostings',
+            'month': '2016-07',
+            'purpose': 'testing'
+        }}
+
+def retry_if_io_error(exception):
+    return isinstance(exception, IOError)
